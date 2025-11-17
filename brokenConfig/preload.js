4c1bf9e072f588dcd5c1d1c670330fb5ce513298
xmu <147617344+xmuaop@users.noreply.github.com>
2025-11-17 18:50:15 +0100

    brokenConfig accidentel

--- brokenConfig/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electron', {
-  invoke: (channel, data) => ipcRenderer.invoke(channel, data),
-  on: (channel, listener) => ipcRenderer.on(channel, (_event, data) => listener(data))
-})
